937b3ad67a7248ce0a547e8c91b83f44ebff4034
Arjun <arjun2000raj@gmail.com>
2022-10-07 12:11:07 +0530

    doc crated

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,46 @@
+const mongoose = require('mongoose')
+const Document = require('./Document')
+
+const uri = 'mongodb+srv://arjun:mydbpassword@cluster0.djrfc4f.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(db => {
+    console.log('databse connected')
+})
+
 const io = require('socket.io')(3001, {
-    cors:{
+    cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST']
     }
 })
 
+const defaultValue = '';
+
 io.on('connection', socket => {
-    console.log('connected')
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit('load-document', document.data)
+
+        socket.on('send-changes', delta => {
+
+            socket.broadcast.to(documentId).emit('receive-changes', delta)
+        })
+
+        socket.on('save-document', async data => {
+            await Document.findByIdAndUpdate(documentId, {data})
+        })
+    })
 })
+
+async function findOrCreateDocument(id){
+    if(id == null) return
+
+    const document = await Document.findById(id)
+
+    if(document) return document
+
+    return await Document.create({_id: id, data: defaultValue})
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
 import { TextEditor } from "./TextEditor";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { v4 as uuid } from 'uuid';
 
 function App() {
   return (
-      <TextEditor />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuid()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+
+    </Router>
   );
 }
 
